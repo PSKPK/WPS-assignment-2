797d4d2f355afdec8a9f7a21363aff7ad91cb52a
PSKPK <84656575+PSKPK@users.noreply.github.com>
2021-12-11 20:57:22 +0530

    Create valid.java

---
+++ Qsn2/valid.java
@@ -0,0 +1,20 @@
+package random;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Valid143")
+public class Valid extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username=request.getParameter("emailid");		
+		RequestDispatcher rd =request.getRequestDispatcher("home.html");
+		rd.forward(request, response);
+} }
